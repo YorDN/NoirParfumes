72559020125c057e635b4477e5f26c45306be132
Yordan <mixdata34@gmail.com>
2024-11-24 18:32:18 +0200

    did the popular slider and it is finished

---
+++ JavaScript/popular-scroll.js
@@ -0,0 +1,18 @@
+let scrollContainer = document.querySelector('.products-container');
+let backBtn = document.getElementById('backBtn');
+let nextBtn = document.getElementById('nextBtn');
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+  scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 1100;
+});
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 1100;
+});
