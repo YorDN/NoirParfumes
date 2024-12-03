fef9fea68c1316dc62cb7ef80c858b49bf924931
Yordan <mixdata34@gmail.com>
2024-12-03 08:47:41 +0200

    added errors

--- JavaScript/validator.js
+++ JavaScript/validator.js
@@ -1,4 +1,21 @@
 const form = document.getElementById("form");
-const email = document.getElementById("email-input");
-const password = document.getElementById("password-input");
+const email_input = document.getElementById("email-input");
+const password_input = document.getElementById("password-input");
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let errors = []
+    if(firstname_input){
+        errors = getSignupFormErrors(firstname_input.value, email_input.value, password_input.value, repeat_password_input.value);
+    }
+    else{
+        errors = getLoginFormErrors(email_input.value, password_input.value); 
+    }
+});
+function getSignupFormErrors(firsname, email, password, repeatPassword){
+    let errors = []
+
+    if(firsname === '' || firsname == null){
+        errors.push('Първото Ви име е задължително!')
+    }
+}
 
